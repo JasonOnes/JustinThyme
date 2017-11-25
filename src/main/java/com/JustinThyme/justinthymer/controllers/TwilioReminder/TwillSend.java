11486c133e4fd967c410847bd2ab88affbdb9cfa
JasonOnes <jasonr.jones14@gmail.com>
2017-11-24 18:28:36 -0800

    get config vars for twilio

--- src/main/java/com/JustinThyme/justinthymer/controllers/TwilioReminder/TwillSend.java
+++ src/main/java/com/JustinThyme/justinthymer/controllers/TwilioReminder/TwillSend.java
@@ -8,19 +8,17 @@ import com.twilio.type.PhoneNumber;
 public class TwillSend {
 
     // Setup your own account for personal SID, TOKEN, and phone number
-    public static final String ACCOUNT_SID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-    public static final String  AUTH_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-
-
+    public static final String ACCOUNT_SID = System.getenv("ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
 
 
     public static void twill_away(String message, String number) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message twill_message = Message.creator(new PhoneNumber (number),//number to (receiving)
-                new PhoneNumber("XXXXXXXXXXX"),//number from (this will be your Twillio number)
-
+//                new PhoneNumber("XXXXXXXXXXX"),//number from (this will be your Twillio number)
+                new PhoneNumber("5037838539"),
 
                 message).create();//message from form and passed via model
 
